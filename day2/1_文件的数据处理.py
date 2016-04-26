0643e179ebd3c39153e1d117adfba5dfe702f823
NinaGitHubUserName2015 <ninamonkey@126.com>
2016-04-26 17:41:10 +0800

    update file operate function

--- day2/1_文件的数据处理.py
+++ day2/1_文件的数据处理.py
@@ -18,4 +18,13 @@ f.write('second line')        #在原来内容后面追加,没有换行
 f.write('\nthird line')       #换行
 f.flush()
 
+#以写读模式打开,写读模式是说清空这个文件，然后再往里面写
+f = file('hello.txt','w+')
+
+# b代表二进制，在 linux 上面没有用，在 windows 上使用:
+# linux上面换行是 \n , window上面换行是 \n\r;
+# linux上面有一个命令是dos2unix，意思是把 windows 格式写的代码或文本文件转换成linux格式；
+# b可以自动把 \n\r 转化为 \n;
+# 例外：linux上面有一个包 zipfile, 可以直接通过这个包读取 zip包，这个时候必须要加上 b.
+
 f.close
---
+++ day2/2_file_handler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+f = file('hello.txt')      # 默认的打开方式是r
+
+for i in f.readlines():
+	print i
+	print i.strip('\n')     #表示去掉换行符，没有参数 可以去掉换行符，空格，制表符tab
+	print i.strip().split(':')[0]          # 分开,成为一个列表，取列表值
+
+f.name
+f.closed
+f.isatty()  #文件是否是一个终端
+f.next()   #迭代使用，跟readline类似
+f.read()   #以字符串形式读出所有行
+f.readlines()     #以列表形式读出所有行          	
+f.seek(0)       #主要用来跳到文件开始，跳到文件某个位置
+f.tell()        #打出当前在文件的位置
+f.truncate(100)    #从文件开头截断到第100个位置，跟当前所在位置无关
+
+#写多行
+a = range(10)       #从0到10的数字列表
+a = [str(i) for i in a]    #转化为字符串列表
+a = [str(i)+'\n' for i in a]   #加上换行符 
+f.writelines(a)
+
+for line in f.xreadlines():     #逐行读，读取大文件，读一行打印一行,实际上是有一个迭代器
+	print line 
+
+f.close()     
